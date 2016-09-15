4607ef1c65cad0979accbdf8b3c198c5a6bd4082
Zbigniew Pieślak <zpieslak@gmail.com>
2016-09-15 22:42:18 +0200

    Optimize assets

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,58 @@
-var oxyThemeData = {'navbarScrolledPoint':'15','navbarHeight':'110','navbarScrolled':'90','siteLoader':'on','menuClose':'off','scrollFinishedMessage':'No more items to load.','hoverMenu':{'hoverActive':true,'hoverDelay':200,'hoverFadeDelay':200}}
-var map756 = {'map_type':'ROADMAP','map_zoom':'17','map_style':'regular','map_scrollable':'on','marker_link':'http:\/\/lupotc.pl\/images\/android-chrome-48x48.png','marker':'show','auto_center':'1','latlng':['52.087392, 16.662592'],'center_latlng':'','label':['Lupo Plast'],'address':'','height':'400','extra_classes':'','margin_top':'0','margin_bottom':'0','scroll_animation':'none','scroll_animation_delay':'0','hidden_on_large':'off','hidden_on_medium':'off','hidden_on_small':'off','hidden_on_xsmall':'off','markerURL':'http:\/\/lupotc.pl\/images\/android-chrome-48x48.png'}
+---
+layout: null
+---
+
+// Includes
+{% include_relative vendor/jquery.min.js %}
+{% include_relative vendor/bootstrap.min.js %}
+{% include_relative vendor/jquery.isotope.min.js %}
+{% include_relative vendor/jquery.magnific-popup.min.js %}
+{% include_relative vendor/jquery.waypoints.min.js %}
+{% include_relative vendor/imagesloaded.min.js %}
+{% include_relative vendor/jquery.placeholder.min.js %}
+{% include_relative vendor/jquery.scrollto.min.js %}
+{% include_relative vendor/skrollr.min.js %}
+{% include_relative vendor/jquery.fitvids.min.js %}
+{% include_relative vendor/map.min.js %}
+{% include_relative vendor/mediaelement-and-player.min.js %}
+{% include_relative vendor/theme.min.js %}
+
+// Settings
+var oxyThemeData = {
+  navbarScrolledPoint: '15',
+  navbarHeight: '110',
+  navbarScrolled: '90',
+  siteLoader: 'on',
+  menuClose: 'off',
+  scrollFinishedMessage: 'No more items to load.',
+  hoverMenu: {
+    hoverActive: true,
+    hoverDelay: 200,
+    hoverFadeDelay: 200
+  }
+}
+
+var map756 = {
+  map_type: 'ROADMAP',
+  map_zoom: '17',
+  map_style: 'regular',
+  map_scrollable: 'on',
+  marker: 'show',
+  auto_center: '1',
+  latlng: ['52.087392, 16.662592'],
+  center_latlng: '',
+  label: ['Lupo Plast'],
+  address: '',
+  height: '400',
+  extra_classes: '',
+  margin_top: '0',
+  margin_bottom: '0',
+  scroll_animation: 'none',
+  scroll_animation_delay: '0',
+  hidden_on_large: 'off',
+  hidden_on_medium: 'off',
+  hidden_on_small: 'off',
+  hidden_on_xsmall: 'off',
+  marker_link: 'http:\/\/lupotc.pl\/images\/android-chrome-48x48.png',
+  markerURL: 'http:\/\/lupotc.pl\/images\/android-chrome-48x48.png'
+}
